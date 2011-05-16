db948dd1f5768a4fee888743429623724385cacc
Jared Mooring <jared.mooring@gmail.com>
2011-05-17 08:52:16 +1000

    Requires a list of plugins to be defined in the config in order to load

--- bootstrap.php
+++ bootstrap.php
@@ -1,18 +1,25 @@
 <?php
 
-define('DEBUG', false);
+define('DEBUG', true);
 
 function __autoload($class_name) {
+    global $config;
 	$filename = 'class/'.strtolower($class_name).'.class.php';
 	print "looking for $filename\n";
 	if (!file_exists($filename)) {
 		$filename = 'plugins/'.strtolower($class_name).'.plugin.php';
-		print "looking for $filename\n";
-		if (!file_exists($filename)) {
-			throw new Exception("Unable to load $class_name.");
-			return false;
-		}
+        print "looking for $filename\n";
+        if (!file_exists($filename)) {
+            throw new Exception("Unable to load $class_name.");
+            return false;
+        }
+
+        if(@in_array($class_name, $config['plugins']) !== true) {
+            print "skipping $class_name\n";
+            return ;
+        }
 	}
-	include_once($filename);
+
+    include_once($filename);
 }
 
