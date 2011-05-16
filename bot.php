5ead7e04b056188a386b7bc528cf29ac79349b44
Jared Mooring <jared.mooring@gmail.com>
2011-05-17 08:53:05 +1000

    Added check for config.php. Reversed order of bootstrap and config include

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 if(file_exists('config.php') !== true)
     die("Doh! It looks like you haven't created your own config.php file. Please copy config.php-default to config.php and make your changes");
 
-require_once('bootstrap.php');
 require_once('config.php');
+require_once('bootstrap.php');
 
 $bot = new SilverBot($config);
 $bot->connect();
